d1e89c9425cecd12733373478a7b55ecbb485436
basil <basel_marwan@icloud.com>
2020-04-22 09:37:15 +0200

    Setting uo basic components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Users from './user/pages/user'
+import NewPlace from './places/pages/NewPlace';
 
 const App = () =>{
-  return <Router>
-    <Route Path="/"> 
-      <Users />
-    </Route>
+  return (
+    <Router>
+      <Switch>
+        <Route Path="/" exact> 
+          <Users />
+        </Route>
+        <Route path="/places" >
+          <NewPlace/>
+        </Route>
+      </Switch>
   </Router>
+  );
 };
 
 export default App;

7d3efa1190a2958e14b12dcd94bcdee35b3a25e5
basil <basel_marwan@icloud.com>
2020-04-22 23:23:43 +0200

    Fixed mobile Nav

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import './MainNavigation.css'
 import MainHeader from './MainHeader';
 import { Link } from 'react-router-dom';
 import NavLinks from './NavLinks';
 import SideDrawer from './SideDrawer'
+import Backdrop from '../UIElements/Backdrop';
 
 const MainNavigation = props => {
+    const  [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawer = () => {
+        setDrawerIsOpen(true);
+    } 
+
+    const closeDrawer = () => {
+        setDrawerIsOpen(false)
+    }
+
     return (
         <React.Fragment>
-            <SideDrawer >
+            {drawerIsOpen && <Backdrop onClick ={closeDrawer}/>}
+            {drawerIsOpen && (<SideDrawer >
                 <nav className = "main-navigation__drawer-nav">
                     <NavLinks />
                 </nav>
             </SideDrawer>
+            )}
         <MainHeader>
-            <button className="main-navigation__menu-btn">
+            <button className="main-navigation__menu-btn" onClick = {openDrawer}>
                 <span />
                 <span />
                 <span />
